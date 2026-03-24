3aa97acf98c87403f5d6bd8eef97eae54471800a
Lasse Hansen <lassehhansen02@gmail.com>
2026-03-24 21:42:36 +0100

    Search and Update logic added

--- Inventory.cs
+++ Inventory.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System;
+using System.Diagnostics;
 
 public class Inventory
 {
@@ -18,12 +20,39 @@ public class Inventory
     {
         if (_products.Count == 0)
         {
-            Console.WriteLine("Lageret er tomt.");
+            Console.WriteLine("The inventory is empty");
+            return; // Stop if inventory is empty
         }
 
         foreach (Product p in _products)
         {
             p.DisplayDetails();
+        }
+    }
+
+    public Product FindProductById(int id)
+    {
+        foreach (Product p in _products)
+        {
+            if (p.Id == id)
+            {
+                return p; // Stop when we have found our product
+            }
+        }
+        return null; // If we did not find anything
+    }
+
+    public void UpdateStock(int id, int amount)
+    {
+        Product foundProduct = FindProductById(id);
+        if (foundProduct != null)
+        {
+            foundProduct.StockQuantity += amount;
+            Console.WriteLine($"Inventory got updated with: {foundProduct.Name}");
+        }
+        else
+        {
+            Console.WriteLine($"Failure. Product with ID {id} was not found.");
         }
     }
 
